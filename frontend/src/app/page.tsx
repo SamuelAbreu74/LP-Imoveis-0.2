593cdb560782b3af213e402632320db8e6b6c223
Samuel Abreu <samuelabreudev@gmail.com>
2026-02-25 11:11:12 -0300

    feat: updating the folder structure

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black">
+      
+
+      <main className="flex min-h-screen w-full max-w-3xl flex-col p-8">
+        <h1>testando</h1>
+      </main>
+    </div>
+  );
+}
