bd0f0e8669970dd74ad0a9eae1132d591e15c1d2
Samuel Abreu <samuelabreudev@gmail.com>
2026-06-25 15:30:22 -0300

    feat: Adicionando novos atributos (nome, endereco e url_photo) no types de imoveis

--- frontend/src/types/imovel.ts
+++ frontend/src/types/imovel.ts
@@ -2,12 +2,15 @@ export interface Imovel {
     id: number;
     id_campanha: number;
     id_tipo_imovel: number | null;
-    tamanho_total_m2: number | null;
-    area_construida_m2: number | null;
+    tamanho_total_m2: string | number | null;
+    area_construida_m2: string | number | null;
     comodos: Record<string, any> | null;
     descricao: string | null;
-    valor_venda: number | null;
-    valor_avaliacao_caixa: number | null;
+    valor_venda: string | number | null;
+    valor_avaliacao_caixa: string | number | null;
     status: number | null;
     created_at: string | null;
+    nome?: string | null;
+    endereco?: string | null;
+    url_photo?: string | null; 
 }
