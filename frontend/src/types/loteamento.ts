331c4fba319ef5e1ee8345b40f72347dc97e5441
Samuel Abreu <samuelabreudev@gmail.com>
2026-07-27 18:47:04 -0300

    feat: Atualizando types de loteamentos

--- frontend/src/types/loteamento.ts
+++ frontend/src/types/loteamento.ts
@@ -1,13 +1,16 @@
-export interface Loteamento {
-    id: number;
-    nome_loteamento: string | null;
-    quantidade_unidades: number | null;
-    logradouro: string | null;
-    numero: string | null;
-    bairro: string | null;
-    cidade: string | null;
-    uf: string | null;
-    cep: string | null;
-    created_at: string | null;
-    id_tipo_loteamento: number | null;
+import { ImovelDTO } from './imovel';
+
+export interface LoteamentoDTO {
+  id: number;
+  nome_loteamento: string | null;
+  quantidade_unidades: number | null;
+  logradouro: string | null;
+  numero: string | null;
+  bairro: string | null;
+  cidade: string | null;
+  uf: string | null;
+  cep: string | null;
+  id_tipo_loteamento: number | null;
+  created_at: string | null;
+  imoveis?: ImovelDTO[];
 }
